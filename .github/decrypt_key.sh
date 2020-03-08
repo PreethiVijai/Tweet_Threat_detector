bc45290b7006920d642795e3bf34ce1518c92c9d
Scott Young <scottslyyoung@gmail.com>
2020-03-08 14:45:17 -0600

    Create decrypt_key.sh

---
+++ .github/decrypt_key.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase=${{secrets.ENCRYPT_KEY}} \
+--output $HOME/secrets/key.json secrets/idyllic-kit-269502-325031fc9cf7.json.gpg
