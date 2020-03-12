76645c52e4ee0d9eebceb8de2db0745d261f8cd7
Scott Young <scottslyyoung@gmail.com>
2020-03-11 19:07:42 -0600

    Update decrypt_key.sh

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --batch --yes --decrypt --passphrase="password" \
---output $HOME/secrets/key.json .secrets/test.txt.gpg #.secrets/idyllic-kit-269502-325031fc9cf7.json.gpg
+gpg --batch --yes --decrypt --passphrase="$PASSPHRASE" \
+--output $HOME/secrets/key.json .secrets/idyllic-kit-269502-325031fc9cf7.json.gpg
