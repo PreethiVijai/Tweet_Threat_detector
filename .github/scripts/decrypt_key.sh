8f695729b88a54894781bca85961489fd458df23
scottlyoung <scottslyyoung@gmail.com>
2020-03-11 18:31:44 -0600

    reorginized file structure

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --batch --yes --decrypt --passphrase="$secrets.ENCRYPT_KEY" \
---output $HOME/secrets/key.json secrets/idyllic-kit-269502-325031fc9cf7.json.gpg
+--output $HOME/secrets/key.json .secrets/idyllic-kit-269502-325031fc9cf7.json.gpg
