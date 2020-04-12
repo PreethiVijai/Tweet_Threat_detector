7cb7f8dc25b20717ba1c42717e9e7940b76f1259
scottlyoung <scottslyyoung@gmail.com>
2020-04-12 14:15:18 -0600

    Added skeletons for threat object and Database access class to analyzer

---
+++ application/Analyzer/src/threat.py
@@ -0,0 +1,9 @@
+
+
+class threat:
+    def __init__(self, _ID, _type, _location, _confidence, _tweets):
+        self.ID = _ID
+        self.type = _type
+        self.location = _location
+        self.confidence = _confidence
+        self.tweets = _tweets
