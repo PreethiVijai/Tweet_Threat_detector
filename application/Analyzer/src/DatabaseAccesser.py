bc7b0fcef45632ed993ac5b26f68f5f59c86f81d
scottlyoung <scottslyyoung@gmail.com>
2020-04-14 08:24:25 -0600

    Implemented database accesser, tests pending

--- application/Analyzer/src/DatabaseAccesser.py
+++ application/Analyzer/src/DatabaseAccesser.py
@@ -1,8 +1,31 @@
 
+from datetime import date
+import mysql.connector
+import json
+
+
+add_threat = ("INSERT INTO threats "
+              "(id, type, location, confidence, tweets, date) "
+              "VALUES (%(id)i, %(type)s, %(location)s, %(confidence)f, %(tweets)s, %(date)s)")
+
 
 class DatabaseAccesser:
-    def __init__(self, address):
-        pass
+    def __init__(self, address, database):
+        self.connection = mysql.connector.connect(user="", password="", host=address, database=database)
 
     def add_threat(self, threat):
-        pass
+        cursor = self.connection.cursor()
+        data_threat = {
+            'id': threat.ID,
+            'type': threat.type,
+            'location': threat.location,
+            'confidence': threat.confidence,
+            'tweets': json.dumps(threat.tweets),
+            'date': date.today()
+        }
+        cursor.execute(add_threat, data_threat)
+        self.connection.commit()
+        cursor.close()
+
+    def shutdown(self):
+        self.connection.close()
