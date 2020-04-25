32606b75822c169d0f5338b09c5f16b89494e0b3
PreethiVijai <prvi9376@colorado.edu>
2020-04-25 07:44:22 -0600

    react

--- application/App/src/threat-detector-ui/threat-detector-ui-launch.sh
+++
@@ -1,4 +0,0 @@
-npm run-script build
-docker build -t gcr.io/idyllic-kit-269502/app:latest/reactui:v1 .
-docker push gcr.io/idyllic-kit-269502/app:latest/reactui:v1
-kubectl apply -f deployment.yaml
