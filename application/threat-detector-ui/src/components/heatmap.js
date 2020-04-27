5bb53ebcfa6067b91f31d47bff3f656155df3822
PreethiVijai <prvi9376@colorado.edu>
2020-04-27 10:56:25 -0600

    HeatMap

--- application/threat-detector-ui/src/components/heatmap.js
+++ application/threat-detector-ui/src/components/heatmap.js
@@ -1,37 +1,136 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { tweets } from './UserFunctions'
 import USAMap from "react-usa-map";
-class USAmap extends Component {
-  tweet_location = [];
-  results = {};
+
+class App extends Component {
+  /* mandatory */
+
+  constructor(props) {
+      super(props);
+  this.state = {
+  tweet_location_abbr : [],
+  results :{},
+  tweet_arr :[],
+  location: '',
+  tweet_type:[],
+  tweet_data:[],
+  tweet_location : []
+}
+}
+
   mapHandler = (event) => {
     alert(event.target.dataset.name);
   };
+  mapStates=(event)=>{
+    const us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+var i = 0
 
-  componentDidMount() {
-    const stateName1 = "#66fcf1";
-    this.tweet_location = this.props.tweet_location;
-    this.results = {};
+for(i = 0; i < this.state.tweet_location.length; i++){
+    this.state.tweet_location_abbr[i]=us_state_abbrev[this.state.tweet_location[i]]
+}
+}
 
-    console.log(this.tweet_location);
-    if (this.tweet_location.length > 0) {
-      var i;
-      for (i = 0; i < this.tweet_location.length; i++) {
-        this.results[this.tweet_location[i]] = { fill: stateName1 };
+
+  /* optional customization of filling per state and calling custom callbacks per state */
+   componentDidMount() {
+     tweets().then(res => {
+       if (!res.error) {
+         this.setState( {tweet_arr: res })}
+         const obj=this.state.tweet_arr
+         console.log(obj)
+         Object.keys(obj).map((index) => {
+           this.state.tweet_type[index] = obj[index].type
+           this.state.tweet_location[index] = obj[index].location
+           this.state.tweet_data[index]=obj[index].tweets
+
+           this.mapStates()
+           console.log("inside mount")
+           console.log(this.state.tweet_location_abbr)
+         })
+
+         })
+}
+  statesCustomConfig = () => {
+    const fill_color = "#CC0000"
+    console.log("inside statesCustomConfig")
+
+    if (this.state.tweet_location_abbr.length > 0) {
+        var i;
+
+        for (i = 0; i < this.state.tweet_location_abbr.length; i++) {
+          this.state.results[this.state.tweet_location_abbr[i]] = { fill: fill_color };
+        }
       }
-    }
-    console.log(this.results)
-    return this.results;
-  }
+
+      console.log(this.state.results)
+
+      return this.state.results
+  };
 
   render() {
     return (
-      <div className="Usamap">
-        <USAMap
-          customize={this.componentDidMount()}
-          onClick={this.mapHandler}
-        />
+      <div className="App">
+        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
       </div>
     );
   }
 }
-export default USAmap;
+
+export default App;
