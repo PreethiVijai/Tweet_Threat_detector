cc504a784f996e08bb50514c7a3b25d1aa6e9f4e
PreethiVijai <prvi9376@colorado.edu>
2020-04-29 09:23:14 -0600

    buid script changes for flask and react1

--- application/threat-detector-ui/runTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# This is the script you need to provide to launch a reactui service
-#
-npm run-script build
-docker build -t gcr.io/idyllic-kit-269502/threat-detector-ui:v1 .
-docker push gcr.io/idyllic-kit-269502/threat-detector-ui:v1
-kubectl apply -f ../kube_deploy/react_ui_deployment.yaml
